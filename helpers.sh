3a6ac7a19cc25df61171d257f2d802b286a395d6
nu11p01n73R <kjjohny1991@gmail.com>
2017-03-08 10:25:29 +0530

    Added functionalities to - Change Color Scheme - List available Schemes - List Current Scheme - Demo available Schemes'

---
+++ helpers.sh
@@ -0,0 +1,77 @@
+#!/usr/loca/bin/bash
+
+LINK_FILE="$HOME/.color"
+
+#
+# List all the available colors in the format
+# color-background
+# Color names correspond to their shell files in $BASE16_PATH
+#
+function list_all()
+{ 
+        scheme_files="${B_PATH}/scripts/*.sh"
+        for file in $scheme_files
+        do
+                get_color_name "$file"
+        done | column
+}
+
+function get_color_name()
+{
+        echo -e $1 | sed "s#${B_PATH}/scripts/base16-\([^.]*\).sh#\\1#"
+}
+
+function link_scheme()
+{
+        scheme_file="${B_PATH}/scripts/base16-$1.sh"
+        if [[ ! -f "$scheme_file" ]]
+        then
+               echo "Scheme file not found" 
+               return 1
+        fi
+
+        ln -fs "$scheme_file" "$LINK_FILE" 
+}
+
+function load_scheme()
+{
+        link_scheme $1 && source "$LINK_FILE"
+}
+
+function current_scheme()
+{
+        if [[ ! -f "$LINK_FILE" ]]
+        then
+                echo "No color scheme available"
+                return
+        fi
+
+        echo -n "Current Color Scheme : "
+        scheme_file=$(readlink $LINK_FILE)
+        get_color_name "$scheme_file"
+}
+
+function demo()
+{
+        scheme_files="${B_PATH}/scripts/*.sh"
+        for file in $scheme_files
+        do
+                echo -n "Scheme : "
+                get_color_name $file
+                source $file
+                sleep 2
+        done
+}
+
+if [[ $1 == "list" ]]
+then
+        list_all
+elif [[ $1 == "demo" ]]
+then
+        demo
+elif [[ ! -z $1 ]] 
+then
+        load_scheme $1
+else
+        current_scheme
+fi
